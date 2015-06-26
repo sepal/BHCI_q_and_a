0bc1e02c8fc075e4009f556148a2658bcdd78bab
Sebastian <sep.gil@gmail.com>
2015-06-26 15:59:08 +0200

    Moved search components into their own folders.

--- dist/js/app.js
+++ dist/js/app.js
@@ -38665,13 +38665,13 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _componentsSearch = require('./components/Search');
+var _componentsSearchSearch = require('./components/search/Search');
 
-var _componentsSearch2 = _interopRequireDefault(_componentsSearch);
+var _componentsSearchSearch2 = _interopRequireDefault(_componentsSearchSearch);
 
-_react2['default'].render(_react2['default'].createElement(_componentsSearch2['default'], null), $('#app--body').get(0));
+_react2['default'].render(_react2['default'].createElement(_componentsSearchSearch2['default'], null), $('#app--body').get(0));
 
-},{"./components/Search":240,"react":228}],239:[function(require,module,exports){
+},{"./components/search/Search":240,"react":228}],239:[function(require,module,exports){
 "use strict";
 
 var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
